88f944f9a5434a303533d5436e09f1a4c08df94d
Marc Fournier <marc.fournier91@gmail.com>
2016-07-26 19:13:55 -0400

    Added paths for edit and delete.

--- app.rb
+++ app.rb
@@ -22,3 +22,25 @@ get '/pokemons/:id' do
   @pokemon = Pokemon.find(params[:id])
   erb :"pokemons/show"
 end
+
+post '/pokemons' do
+  @pokemon = Pokemon.create(params[:pokemon])
+  redirect "/pokemons/#{@pokemon.id}"
+end
+
+get 'pokemons/:id/edit' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"pokemons/edit"
+end
+
+put '/pokemons/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.update(params[:pokemon])
+  redirect "/pokemons/#{@pokemon.id}"
+end
+
+delete '/pokemons/:id' do
+  pokemon = Pokemon.find(params[:id])
+  pokemon.destroy
+  redirect '/pokemons'
+end
