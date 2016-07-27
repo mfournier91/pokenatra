7868b8fbb0bc998cd2dba9e53a63e9185d968f62
Marc Fournier <marc.fournier91@gmail.com>
2016-07-27 18:27:08 -0400

    Adds paths for show new and edit.

--- app.rb
+++ app.rb
@@ -50,3 +50,27 @@ get '/trainers' do
   @all_trainers = Trainer.all
   erb :"trainers/index"
 end
+
+get '/trainers/new' do
+  erb :"trainers/new"
+end
+
+get '/trainers/:id' do
+  @trainer = Trainer.find(params[:id])
+  erb :"trainers/show"
+end
+
+post '/trainers' do
+  @trainer = Trainer.create(params[:trainer])
+  redirect "/trainers/#{@trainer.id}"
+end
+
+get '/trainers/:id/edit' do
+  @trainer = Trainer.find(params[:id])
+  erb :"trainers/edit"
+end
+
+put '/trainers/:id' do
+  @trainer = Trainer.find(params[:id])
+  @trainer.update(params[:trainer])
+end
